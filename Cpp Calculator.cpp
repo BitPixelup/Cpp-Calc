c6469b2986b02e07418e00e1a993a8aa0b71065d
Akshat <107185907+BitPixelup@users.noreply.github.com>
2024-10-26 12:21:55 +0530

    Add files via upload

---
+++ Cpp Calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Adder"<<endl;
+	int num1, num2, adder ;
+
+	cout << "Enter your first Number : ";
+	cin >> num1;
+	cout << "Enter your second Number : ";
+	cin >> num2;
+
+	adder = num1 + num2;
+
+	cout <<"Your sum is " << adder;
+	
+
+}
